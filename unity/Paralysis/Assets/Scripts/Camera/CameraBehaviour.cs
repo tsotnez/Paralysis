2b20d723931e578eb966c74efca0e5f0fa1ab5a8
janHildebrandt98 <jan.hildebrandt98@gmx.de>
2017-09-27 15:28:45 +0200

    Added button to switch current champion

---
+++ unity/Paralysis/Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float offsetX = 0;
+    [SerializeField]
+    float offsetY = 0;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(target.position.x + offsetX, target.position.y + offsetY, transform.position.z);
+	}
+
+    public void changeTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
--- unity/Paralysis/Assets/debug.cs
+++ unity/Paralysis/Assets/debug.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class debug : MonoBehaviour {
+    public GameObject[] champions;
+    int currentChamp = 0;
 
-    
+    //Switches the champion by changing the prefab
+    public void changeChampion()
+    {
+        increaseIndex();
+
+        GameObject currentPlayer = GameObject.FindGameObjectWithTag("MainPlayer");
+
+        Debug.Log(currentPlayer.name);
+        GameObject newPlayer = Instantiate(champions[currentChamp], currentPlayer.transform.position, Quaternion.identity);
+
+        Camera.main.GetComponent<CameraBehaviour>().changeTarget(newPlayer.transform);
+        Destroy(currentPlayer);
+    }
+
+    private void increaseIndex()
+    {
+        if ((currentChamp + 1) <= (champions.Length - 1)) currentChamp++;
+        else currentChamp = 0;
+    }
 }
