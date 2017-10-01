43951735c3a30fdd04908327228c29c9d8192292
GrewSum <32300372+GrewSum@users.noreply.github.com>
2017-10-01 21:48:32 +0200

    added startchar as debug-option

--- unity/Paralysis/Assets/debug.cs
+++ unity/Paralysis/Assets/debug.cs
@@ -6,6 +6,23 @@ public class debug : MonoBehaviour {
     public GameObject[] champions;
     int currentChamp = 0;
 
+    [SerializeField]
+    private int startChar = 0;
+
+    private void Start()
+    {
+        currentChamp = startChar;
+
+        GameObject currentPlayer = GameObject.FindGameObjectWithTag("MainPlayer");
+
+        Debug.Log(currentPlayer.name);
+        GameObject newPlayer = Instantiate(champions[currentChamp], currentPlayer.transform.position, Quaternion.identity);
+
+        Camera.main.GetComponent<CameraBehaviour>().changeTarget(newPlayer.transform);
+        Destroy(currentPlayer);
+
+    }
+
     //Switches the champion by changing the prefab
     public void changeChampion()
     {
