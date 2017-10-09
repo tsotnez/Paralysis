96749562ae6538769b6f09667b66bfd0dc1a1c1e
manuel <manuelbenites7@gmail.com>
2017-10-09 02:54:33 +0200

    animation MainMenu update

--- unity/Paralysis/Assets/Scripts/MainMenu/MainMenuController.cs
+++ unity/Paralysis/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -21,6 +21,13 @@ public class MainMenuController : MonoBehaviour
 
 
     public void ChangeScene(string sceneName){
+        StartCoroutine(ChangeAnimation(sceneName));
+
+    }
+     
+    IEnumerator ChangeAnimation(string sceneName){
+
+        yield return new WaitForSeconds(1.0f);
         SceneManager.LoadScene(sceneName);
     }
 }
