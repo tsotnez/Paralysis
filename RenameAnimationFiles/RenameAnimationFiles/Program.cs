8e48002f6f116ea87a7d90b16d08d93b603918b2
janHildebrandt98 <jan.hildebrandt98@gmx.de>
2017-10-21 16:28:39 +0200

    Added C# Project for renaming script

---
+++ RenameAnimationFiles/RenameAnimationFiles/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameAnimationFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path of Skin Folder containing the animation files:");
+            string path = Console.ReadLine();
+
+            DirectoryInfo info = new DirectoryInfo(path);
+            DirectoryInfo[] sub = info.GetDirectories();
+
+            foreach (DirectoryInfo item in sub.ToList())
+            {
+                int i = 0;
+                foreach (FileInfo file in item.GetFiles("*.png").ToList())
+                {
+                    string numberOfImage;
+                    if (i < 10) numberOfImage = "0" + i;
+                    else numberOfImage = i.ToString();
+
+                    string destination = item.FullName + "\\" + item.Name + "_" + numberOfImage + ".png";
+
+                    File.Move(file.FullName, destination);
+                    i++;
+                }
+
+                i = 0;
+                foreach (FileInfo file in item.GetFiles("*.meta").ToList())
+                {
+                    string numberOfImage;
+                    if (i < 10) numberOfImage = "0" + i;
+                    else numberOfImage = i.ToString();
+
+                    string destination = item.FullName + "\\" + item.Name + "_" + numberOfImage + ".meta";
+
+                    File.Move(file.FullName, destination);
+                    i++;
+                }
+            }
+        }
+    }
+}
