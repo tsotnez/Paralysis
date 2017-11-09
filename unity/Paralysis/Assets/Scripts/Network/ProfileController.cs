0b8be00ec75759395d6d027d10262ac9859f9340
manuel <manuelbenites7@gmail.com>
2017-11-09 02:53:48 +0100

    Network

---
+++ unity/Paralysis/Assets/Scripts/Network/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileController : MonoBehaviour {
+
+    /// <summary>
+    /// Start this instance.
+    /// </summary>
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
