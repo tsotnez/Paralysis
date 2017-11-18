79cb63a526cf11dce6d2e1335a10220f11d9fb0d
GrewSum <32300372+GrewSum@users.noreply.github.com>
2017-11-18 21:09:34 +0100

    Added Trinkets to ChampionClassController and added Array of Trinkets

--- unity/Paralysis/Assets/Scripts/Trinkets/Templates/Trinket.cs
+++ unity/Paralysis/Assets/Scripts/Trinkets/Templates/Trinket.cs
@@ -1,6 +1,21 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
 public abstract class Trinket : MonoBehaviour
 {
     public string DisplayName;
+
+    public enum Trinkets
+    {
+        UseTrinket_GetImmun,
+        UseTrinket_HealingOverTime,
+        UseTrinket_IncreaseDamage,
+        UseTrinket_IncreaseMovementSpeed,
+        UseTrinket_ReflectDamage,
+        UseTrinket_RemoveStatusEffect,
+        UseTrinkets_NextHitDealsbleeding,
+        PassiveTrinket_ChanceDealingMoreDamage,
+        PassiveTrinket_ChanceToStun,
+        PassiveTrinket_SpeedWhenHitted
+    }
 }
