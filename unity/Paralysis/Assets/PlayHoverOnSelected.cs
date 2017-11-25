0578d91119dbf120c6dfade94fe848ee2099bf95
janHildebrandt98 <hasbachtal104>
2017-11-25 19:32:07 +0100

    Main Menu

---
+++ unity/Paralysis/Assets/PlayHoverOnSelected.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayHoverOnSelected : MonoBehaviour {
+
+	public void MouseEnter()
+    {
+        transform.Find("Hover").gameObject.SetActive(true);
+    }
+
+    public void MouseExit()
+    {
+        transform.Find("Hover").gameObject.SetActive(false);
+    }
+}
