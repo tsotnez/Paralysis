5482c5fba57ba1ca061be7053dc02219cf0a429b
Kyle Willett <kyle.willett@bluvector.io>
2018-01-09 16:05:20 -0500

    Added Test scene to jump up through certain areas of map

---
+++ unity/Paralysis/Assets/Scripts/Utils/ShowBoxCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBoxCollider : MonoBehaviour {
+
+	public Color color = Color.red;
+
+	void OnDrawGizmos()
+	{
+		BoxCollider2D bCollider = GetComponent<BoxCollider2D>();
+		Gizmos.color = color;
+		Gizmos.DrawWireCube(bCollider.bounds.center, new Vector3(bCollider.bounds.size.x, bCollider.bounds.size.y));
+	}
+}
