2dfad9462d54c59c51209e03c2df2133b1b43546
janHildebrandt98 <hasbachtal104>
2018-01-11 22:04:42 +0100

    created CastBar

---
+++ unity/Paralysis/Assets/CastBarEndBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CastBarEndBehaviour : MonoBehaviour {
+
+    public Image bar;
+
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 nextPos = new Vector3();
+        nextPos.y = transform.localPosition.y;
+        nextPos.z = transform.localPosition.z;
+        nextPos.x = bar.rectTransform.rect.width * bar.fillAmount;
+
+        transform.localPosition = nextPos;
+    }
+}
