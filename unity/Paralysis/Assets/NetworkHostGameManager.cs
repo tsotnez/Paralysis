1ef8c3c6c9e6703a47b861e12add4d3d766a66cb
janHildebrandt98 <hasbachtal104>
2018-03-16 18:44:38 +0100

    ...

---
+++ unity/Paralysis/Assets/NetworkHostGameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NetworkHostGameManager : UIManager {
+
+    public GameObject firstSelected;
+
+    protected override void Start()
+    {
+        base.Start();
+        EventSystem.current.firstSelectedGameObject = firstSelected;
+    }
+}
